804810d08c494803fd254a1c5e98f5a42d942941
Gustave Curtil <gustave.curtil@tutanota.com>
2025-03-22 22:27:37 +0100

    interactief menu

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ openClose.addEventListener('click', () => {
 });
 
 let elementen = document.querySelectorAll('g');
+let titels = document.querySelectorAll('#titel div')
+let vensters = document.querySelectorAll('#venster div')
+let welkom = document.querySelectorAll('.welkom');
+let infos = document.querySelectorAll('.info');
+let fotos = document.querySelectorAll('.fotos');
+let brieven = document.querySelectorAll('.brieven');
 
 elementen.forEach(element => {
     element.addEventListener('click', () => {
@@ -19,6 +25,48 @@ elementen.forEach(element => {
         elementen.forEach(el => el.classList.remove('actief'));
         if (!isActive) {
             element.classList.add('actief');
+            if (element.id === 'bottom') {
+                console.log('yo')
+                titels.forEach(titel => {
+                    titel.classList.remove('actief')
+                });
+                vensters.forEach(venster => {
+                    venster.classList.remove('actief')
+                });
+                brieven.forEach(brief => {
+                    brief.classList.add('actief') 
+                });
+            } else if (element.id === 'middle') {
+                titels.forEach(titel => {
+                    titel.classList.remove('actief')
+                });
+                vensters.forEach(venster => {
+                    venster.classList.remove('actief')
+                });
+                fotos.forEach(foto => {
+                    foto.classList.add('actief') 
+                });
+            } else if (element.id === 'top') {
+                titels.forEach(titel => {
+                    titel.classList.remove('actief')
+                });
+                vensters.forEach(venster => {
+                    venster.classList.remove('actief')
+                });
+                infos.forEach(info => {
+                    info.classList.add('actief') 
+                });
+            }
+        } else if (isActive) {
+            titels.forEach(titel => {
+                titel.classList.remove('actief')
+            });
+            vensters.forEach(venster => {
+                venster.classList.remove('actief')
+            });
+            welkom.forEach(w => {
+                w.classList.add('actief') 
+            });
         }
     });
 });
